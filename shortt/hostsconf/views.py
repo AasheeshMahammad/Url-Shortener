eb659f7ff0c5e4dd321c6db820f93c5cbdbf1bf4
Aasheesh Mahammad <54064611+AasheeshMahammad@users.noreply.github.com>
2022-07-17 17:39:28 +0530

    changes to view.py

--- shortt/hostsconf/views.py
+++ shortt/hostsconf/views.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.http import HttpResponseRedirect
 
-DEFAULT_REDIRECT_PATH = getattr(settings, 'DEFAUTL_REDIRECT_URL','https://diminuative.herokuapp.com/')
+DEFAULT_REDIRECT_PATH = getattr(settings, 'DEFAUTL_REDIRECT_URL','http://www.shortt.co')
 
 def wildcard_redirect(request, path=None):
     new_url = DEFAULT_REDIRECT_PATH
